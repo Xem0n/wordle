42f6f1e0c5c159ef0debd24542c9bdfaaec4b4e6
Xem0n <biedukiewicz87@gmail.com>
2022-02-10 13:52:23 +0100

    import and initialize user and game

--- js/main.js
+++ js/main.js
@@ -1,1 +1,28 @@
 'use strict';
+
+import User from './modules/user.js';
+import Game from './modules/game.js';
+
+const board = document.querySelector('.board');
+const loading = document.querySelector('.loading');
+
+const showLoading = () => {
+    board.classList.add('hide');
+    loading.classList.remove('hide');
+};
+
+const hideLoading = () => {
+    board.classList.remove('hide');
+    loading.classList.add('hide');
+};
+
+(async () => {
+    showLoading();
+
+    const user = new User();
+
+    const game = await new Game();
+    game.createBoard(6, 5);
+
+    hideLoading();
+})();
